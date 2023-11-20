e468ada62ed2bdd82d2a9fc1c9c6c3dfea040d29
1U21IT028-DIVYADHARSHINI <122510691+1U21IT028-DIVYADHARSHINI@users.noreply.github.com>
2023-11-20 12:32:39 +0530

    Update main.java

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
-# Importing the Suboprocess module
-import subprocess
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-# running command
-command = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
-profiles = [i.split(":")[1][1:-1] for i in command if "All User Profile" in i]
-for i in profiles:
-    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('latin-1').split('\n')
-    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-    try:
-        print("{:<30}|  {:<}".format(i, results[0]))
-    except IndexError:
-        print("{:<30}|  {:<}".format(i, ""))
-input("")
+public class WifiPasswordExtractor {
 
+    public static void main(String[] args) {
+        try {
+            // Running command
+            ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "profiles");
+            Process process = processBuilder.start();
+
+            // Reading the output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder commandOutput = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                commandOutput.append(line).append("\n");
+            }
+
+            // Extracting profiles
+            String[] profiles = commandOutput.toString().split("\n");
+            for (String profile : profiles) {
+                if (profile.contains("All User Profile")) {
+                    String profileName = profile.split(":")[1].trim();
+                    String keyResults = getWifiKey(profileName);
+                    System.out.printf("%-30s|  %-20s%n", profileName, keyResults);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Keep the console open
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getWifiKey(String profileName) throws IOException {
+        // Running command to get key
+        ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "profile", profileName, "key=clear");
+        Process process = processBuilder.start();
+
+        // Reading the output
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder keyOutput = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            keyOutput.append(line).append("\n");
+        }
+
+        // Extracting key content
+        String[] results = keyOutput.toString().split("\n");
+        for (String result : results) {
+            if (result.contains("Key Content")) {
+                return result.split(":")[1].trim();
+            }
+        }
+
+        return "";
+    }
+}
